fd84b50a32cda097b870c452e7a59a239b2ea195
Liam Yafuso <liamyafuso@gmail.com>
2014-09-20 18:28:29 -0700

    added customized vim colorscheme and skeleton of setup file

---
+++ broken_setup.sh
@@ -0,0 +1,29 @@
+url() {
+  echo "https://raw.githubusercontent.com/ehrenmurdick/config/master/$1"
+}
+
+install() {
+  curl -s $(url $1) > ~/.$1
+}
+
+backup() {
+  cp ~/.$1 ~/.$1~
+}
+
+safeInstall() {
+  echo "Installing $1..."
+  if [ -f ~/.$1 ]; then
+    echo "File ~/.$1 exists. Backing it up to ~/.$1~"
+    backup $1
+  fi
+  install $1
+  return 0;
+}
+
+safeInstall 'vimrc'
+safeInstall 'gvimrc'
+safeInstall 'irbrc'
+
+echo 'Installing bundles...'
+vim +PluginInstall +qa
+echo
