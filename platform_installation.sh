d5cd826b07d6ba5df21ec6ad07e9aa855ee20190
LiamY <yafusinator@gmail.com>
2015-02-21 14:40:40 -0800

    Update and rename installation_script.sh to platform_installation.sh

---
+++ platform_installation.sh
@@ -0,0 +1,12 @@
+# ruby and rails installation
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+
+# homebrew installation, the mac package manager
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# python and django installation
+
+# node installation
+
+
+# PostgreSQL Database installation
