c16b28d3d9aa9dfabfe1c940e651281f97741cc7
LiamY <yafusinator@gmail.com>
2015-02-20 20:59:26 -0800

    Update config_setup.sh

--- config_setup.sh
+++ config_setup.sh
@@ -3,9 +3,19 @@ base_url() {
   echo "https://raw.githubusercontent.com/liamy/rig_setup/master/$1"
 }
 
-install() {
+installHidden() {
   # downloads file from rig_setup repo
   curl $(base_url $1) > ~/.$1
+}
+
+install() {
+  # downloads file from rig_setup repo
+  curl $(base_url $1) > ~/$1
+}
+
+backupHidden() {
+  # stores previous settings in a temp file
+  cp ~/.$1 ~/.$1~
 }
 
 backup() {
@@ -13,17 +23,17 @@ backup() {
   cp ~/$1 ~/$1~
 }
 
-backupInstall() {
+backupInstallHidden() {
   echo "Installing $1..."
   if [ -f ~/.$1 ]; then
-    echo "File ~/$1 exists. Backing it up to ~/$1~"
+    echo "File ~/.$1 exists. Backing it up to ~/.$1~"
     backup $1
   fi
   install $1
   return 0;
 }
 
-backupInstall '.vimrc'
+backupInstallHidden 'vimrc'
 
 echo 'Installing bundles...'
 vim +PluginInstall +qa
