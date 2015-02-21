9a54b73f532ba4aeffbb67608243bc422b1b9737
LiamY <yafusinator@gmail.com>
2015-02-21 14:43:02 -0800

    Update platform_installation.sh

--- platform_installation.sh
+++ platform_installation.sh
@@ -1,7 +1,13 @@
 # ruby and rails installation
+echo '########################################################'
+echo '###           INSTALLING RUBY AND RAILS              ###'
+echo '########################################################'
 \curl -sSL https://get.rvm.io | bash -s stable --rails
 
 # homebrew installation, the mac package manager
+echo '########################################################'
+echo '###              INSTALLING HOMEBREW                 ###'
+echo '########################################################'
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # python and django installation
